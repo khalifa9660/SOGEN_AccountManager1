b1e6408a7e381a36eee20c67f28ac909d2bdad62
khali <asura.9660@gmail.com>
2024-02-27 09:13:49 +0100

    New project

---
+++ SoGen_AccountManager1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace SoGen_AccountManager1.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
