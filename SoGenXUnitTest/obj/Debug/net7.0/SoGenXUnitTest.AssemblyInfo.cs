1d44ab326de296446988562bae4fb578e3040b53
khali <asura.9660@gmail.com>
2024-08-29 10:26:41 +0200

    Recovery data

--- SoGenXUnitTest/obj/Debug/net7.0/SoGenXUnitTest.AssemblyInfo.cs
+++ SoGenXUnitTest/obj/Debug/net7.0/SoGenXUnitTest.AssemblyInfo.cs
@@ -13,15 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SoGenXUnitTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-<<<<<<< HEAD
-<<<<<<< HEAD
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d53ee7e4d72935c453c0b04c82e581e7d6b1cb0")]
-=======
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a883a35c2646d2234dc1d5ccd4d1352dc8a9b4d8")]
->>>>>>> origin/dev
-=======
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b621ab1299743295d8b236bef45fd21291d8d28")]
->>>>>>> origin/dev
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2edae61ca63ea6f3da2ad1c30b087151467f2ce5")]
 [assembly: System.Reflection.AssemblyProductAttribute("SoGenXUnitTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SoGenXUnitTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
